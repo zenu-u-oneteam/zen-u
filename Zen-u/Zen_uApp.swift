cf7fa737659844a76d349f11160068bc27e91840
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-04-17 16:38:43 +0530

    Initial Commit

---
+++ Zen-u/Zen_uApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zen_uApp.swift
+//  Zen-u
+//
+//  Created by Sitanshu Pokalwar on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Zen_uApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
