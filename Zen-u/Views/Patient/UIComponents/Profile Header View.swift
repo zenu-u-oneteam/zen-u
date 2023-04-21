45ee65f3d34f223453c3b0d5f405ee069089d61e
Aindrila Ray <aindrilaray0803@gmail.com>
2023-04-21 14:08:25 +0530

    profile header view page setup

---
+++ Zen-u/Views/Patient/UIComponents/Profile Header View.swift
@@ -0,0 +1,20 @@
+//
+//  Profile Header View.swift
+//  Zen-u
+//
+//  Created by Aindrila Ray on 21/04/23.
+//
+
+import SwiftUI
+
+struct Profile_Header_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Profile_Header_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile_Header_View()
+    }
+}
