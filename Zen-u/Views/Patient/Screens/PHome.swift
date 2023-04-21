ac09d7bb3c4e3a7061c6cac264080954ffe0eda1
Harsh Kelawala <harsh.kelawala@gmail.com>
2023-04-21 22:14:04 +0530

    HMS-63 appointment sneakpeaks

--- Zen-u/Views/Patient/Screens/PHome.swift
+++ Zen-u/Views/Patient/Screens/PHome.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct PHome: View {
     var body: some View {
-        VStack {
-            ZStack(alignment: .topLeading){
-                Rectangle().foregroundColor(Color(red: 236/255, green: 236/255, blue: 236/255))
-                    .frame(width: 393,height: 344)
-                    .cornerRadius(50)
+        VStack() {
+          
+//                Rectangle().foregroundColor(Color(red: 236/255, green: 236/255, blue: 236/255))
+//                    .frame(width: 393,height: 344)
+//                    .cornerRadius(50)
                 
                 VStack(alignment: .leading,spacing: 20) {
                     HStack(spacing:40){
@@ -108,21 +108,117 @@ struct PHome: View {
                         
                         
                         
-                    }.padding(.leading,24)
+                    }
+               
+                .padding(.leading,24)
+                .background(Rectangle().foregroundColor(Color(red: 236/255, green: 236/255, blue: 236/255))
+                    .frame(height: 344)
+                    .cornerRadius(50))
+                .ignoresSafeArea()
                     
                     
-                }
-            .ignoresSafeArea()
+            
+            VStack(alignment: .leading,spacing: 20){
+                Text("Upcoming Appointments")
+                    .font(.system(size: 17,weight: .semibold))
+                
+                   
+                    VStack(spacing: -5){
+                        HStack{
+                            Text("PSV23 (Pneumo)")
+                            .font(.system(size: 17,weight: .semibold))
+                            .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                            
+                            Text("9:30")
+                                .font(.system(size: 34,weight:.bold)).foregroundColor(Color.white)
+                            
+                        }
+                        HStack{
+                            AppTypeTag(labetText: "Vaccination")
+                                .foregroundColor(Color(red: 24/255, green: 119/255, blue: 242/255))
+                             Spacer()
+                            Text("Dr.Hanna Fiegel")
+                            .font(.system(size: 15,weight: .bold))
+                            .foregroundColor(Color.white)
+                            
+                        }
+                    }.padding(16)
+                    .background(RoundedRectangle(cornerRadius: 17).fill(Color.blue))
+                    
+                
+                VStack(spacing: -5){
+                    HStack{
+                        Text("Viral Infection")
+                        .font(.system(size: 17,weight: .semibold))
+                        .foregroundColor(.black)
+                        
+                        Spacer()
+                        
+                        
+                        Text("10:00")
+                            .font(.system(size: 34,weight:.bold)).foregroundColor(Color.black)
+                        
+                    }
+                    HStack{
+                        AppTypeTag(labetText: "General")
+//                            .foregroundColor(Color(red: 24/255, green: 119/255, blue: 242/255))
+                         Spacer()
+                        Text("Dr. Ashutosh Malhotra")
+                        .font(.system(size: 15,weight: .bold))
+                        .foregroundColor(Color.black)
+                        
+                    }
+                }.padding(16)
+                    .background(RoundedRectangle(cornerRadius: 17).fill(Color(red: 240/255, green: 240/255, blue: 240/255)))
+                
+    //Color(red: 24/255, green: 119/255, blue: 242/255)
+                
+            }.padding(.init(top: 0, leading: 24, bottom: 24, trailing: 24))
+
+//
+//                HStack{
+//                    VStack(alignment: .leading){
+//                        Text("PSV23 (Pneumo)")
+//                            .font(.system(size: 17,weight: .semibold))
+//                            .foregroundColor(.white)
+//
+//                        AppTypeTag(labetText: "General")
+//
+//
+//                    }
+//                    Spacer()
+//                    VStack(alignment: .trailing){
+//                        Text("9:30").font(.system(size: 34,weight: .bold)).foregroundColor(Color.white)
+//                        Text("Dr.Hannel Fiegele").font(.system(size: 15,weight: .bold)).foregroundColor(Color.white)
+//
+//                    }
+//                }.padding(24)
+          
             
                 Spacer()
+            
                 
                 
-            }
+        }
         }
     }
 
 struct PatientHomeScreens_Previews: PreviewProvider {
     static var previews: some View {
         PHome()
+    }
+}
+
+struct AppTypeTag: View {
+    @State var labetText : String
+    var body: some View {
+        Text(labetText)
+            .padding(8)
+            .font(.system(size: 13,weight: .semibold))
+           //.foregroundColor(Color(red: 24/255, green: 119/255, blue: 242/255))
+            .background(RoundedRectangle(cornerRadius: 5).fill(.white).frame(height: 24))
     }
 }
