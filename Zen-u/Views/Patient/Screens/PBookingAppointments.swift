73a606932d21239dc15204694b8e337495c74f2a
Tanvi129 <tanvigupta129@gmail.com>
2023-04-21 14:37:59 +0530

    HMS-81 UI completed

--- Zen-u/Views/Patient/Screens/PBookingAppointments.swift
+++ Zen-u/Views/Patient/Screens/PBookingAppointments.swift
@@ -15,120 +15,96 @@ var buttonColor = #colorLiteral(red: 0.1882352941, green: 0.5333333333, blue: 1,
 
 struct PBookingAppointments: View {
     @State var symtomText: String = ""
+    @State var showModel: Bool = false
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading){
-                Text("Booking\nConsultation").font(.system(size: 35 , weight : .bold)).padding(.bottom,40)
-    //            HStack(alignment: .lastTextBaseline, spacing: 24){
-    //                Button {
-    //
-    //                } label: {
-    //                    Image(systemName: "arrow.left")
-    //                        .frame(width: 22.0, height: 22.0)
-    //                }
-    //                Spacer()
-    //
-    //                Button {
-    //
-    //                } label: {
-    //                    Image(systemName: "phone").foregroundColor(Color.black).frame(width: 20.0, height: 20.0)
-    //                }.background {
-    //                    Circle().fill(Color(color))
-    //                        .frame(width: 40, height: 40
-    //                        )
-    //                }
-    //
-    //
-    //                Button {
-    //
-    //                } label: {
-    //                    Image(systemName: "person.fill").foregroundColor(Color.black).frame(width: 20.0, height: 20.0)
-    //                }.background {
-    //                    Circle().fill(Color(color))
-    //                        .frame(width: 40, height: 40
-    //                        )
-    //                }
-    ////                .frame(maxWidth: .infinity, alignment: .trailing)
-    //            }.padding(.bottom , 20)
-//                Text("Booking Appointments")
-//                    .fontWeight(.bold)
-//                    .font(.custom("Gilroy-Bold", size: 24))
-//                    .padding(.bottom , 40)
-                Text("Choose Type of Doctor")
-                    .fontWeight(.semibold)
-                    .font(.system(size: 20))
-                
-                    .padding(.bottom , 20)
-                HStack (spacing: 12){
-                    RoundedRectangle(cornerRadius: 60)
-                        
-                        .foregroundColor(Color(doctorTypeBgColor))
-                        .frame(width: 150 , height: 50)
-                        
-                        .overlay( Text("General").font(.custom("Averta-Semibold", size: 14)).foregroundColor(.white)).background {
-                            RoundedRectangle(cornerRadius: 30)
-                                    .stroke(Color.secondary, lineWidth: 4)
-                        }
-                    RoundedRectangle(cornerRadius: 60)
-                        
-                        .foregroundColor(Color(color))
-                        .frame(width: 150 , height: 50)
-                        
-                        .overlay( Text("Specialist")
-                            .font(.custom("Averta-Semibold", size: 14))
-                            .foregroundColor(Color(doctorTypeBgColor)))
-                        .background {
-                            RoundedRectangle(cornerRadius: 30)
-                                    .stroke(Color(doctorTypeBgColor), lineWidth: 4)
-                        }
+            ZStack {
+                VStack(alignment: .leading){
+                    Text("Booking\nConsultation").font(.system(size: 35 , weight : .bold)).padding(.bottom,40)
+    
+                    Text("Choose Type of Doctor")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                    
+                        .padding(.bottom , 20)
+                    HStack (spacing: 12){
+                        RoundedRectangle(cornerRadius: 60)
+                            
+                            .foregroundColor(showModel == false ? Color(doctorTypeBgColor) : Color(color))
+                            .frame(width: 150 , height: 50)
+                            
+                            .overlay( Text("General").font(.custom("Averta-Semibold", size: 14)).foregroundColor(showModel == false ? .white : .black))
+//                            .background {
+//                                RoundedRectangle(cornerRadius: 30)
+////                                        .stroke(Color.secondary, lineWidth: 4)
+//                            }
+                        RoundedRectangle(cornerRadius: 60)
+                            
+                            .foregroundColor(showModel == true ? Color(doctorTypeBgColor) : Color(color))
+                            .frame(width: 150 , height: 50)
+                            
+                            .overlay( Text("Specialist")
+                                .font(.custom("Averta-Semibold", size: 14))
+                                .foregroundColor(showModel == true ? .white : .black))
+//                            .background {
+//                                RoundedRectangle(cornerRadius: 30)
+//                                        .stroke(Color(doctorTypeBgColor), lineWidth: 4)
+//                            }
 
-                }.padding(.bottom , 60)
-                Text("Specify Reason (if Any)")
-                    .fontWeight(.semibold)
-                    .font(.system(size: 20))
-                    .padding(.bottom,30)
-                
-                ZStack (alignment: .leading){
-                    
-                    TextEditor(
-                     
-                      text: $symtomText
-                     
-                    ).padding()
-                    .frame(height: 200.0 )
-                    .colorMultiply(Color(color))
-                    .background(Color(color))
-                    .overlay(
-                            RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color.secondary, lineWidth: 1)
-                )
-                    if symtomText.isEmpty {
-                        VStack  {
-                            Text("Mention any symtoms...").padding(.top,30)
-                                .font(.system(size: 16)).padding(10)
-                                .foregroundColor(Color(hintTextColor))
-                            Spacer()
+                    }.padding(.bottom , 60)
+                        .onTapGesture {
+                            showModel = true
+                            print("Tapped")
                         }
+                    Text("Specify Reason (if Any)")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                        .padding(.bottom,30)
+                    
+                    ZStack (alignment: .leading){
                         
-                    }
-                }.padding(.bottom , 40)
-                   
-                Button {
-                    
-                } label: {
-                    Text("Continue" )
-                        .font(.custom("Averta-Semibold", size: 16))
-                }.padding()
-                    .padding(.horizontal,122)
-                    .background(Color(buttonColor)
-                        .cornerRadius(10))
-                    .foregroundColor(.white)
+                        TextEditor(
+                         
+                          text: $symtomText
+                         
+                        ).padding()
+                        .frame(height: 200.0 )
+                        .colorMultiply(Color(color))
+                        .background(Color(color))
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.secondary, lineWidth: 1)
+                    )
+                        if symtomText.isEmpty {
+                            VStack  {
+                                Text("Mention any symtoms...").padding(.top,30)
+                                    .font(.system(size: 16)).padding(10)
+                                    .foregroundColor(Color(hintTextColor))
+                                Spacer()
+                            }
+                            
+                        }
+                    }.padding(.bottom , 40)
+                       
+                    Button {
+                        
+                    } label: {
+                        Text("Continue" )
+                            .font(.custom("Averta-Semibold", size: 16))
+                    }.padding()
+                        .padding(.horizontal,122)
+                        .background(Color(buttonColor)
+                            .cornerRadius(10))
+                        .foregroundColor(.white)
 
-                
                     
-                    
-            Spacer()
-            }.padding(24)
+                        
+               
+                Spacer()
+    //            SpecialistModelView(isShowing: $showModel)
+                }.padding(24)
+                SpecialistModelView(isShowing: $showModel)
+            }
 //                .navigationTitle(Text("Booking Consultation"))
         }
         
---
+++ Zen-u/Views/Patient/Screens/PDepartmentDetails.swift
@@ -0,0 +1,93 @@
+//
+//  PDepartmentDetails.swift
+//  Zen-u
+//
+//  Created by Tanvi Gupta on 21/04/23.
+//
+
+import SwiftUI
+var deptBackColor = #colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.937254902, alpha: 1)
+var imgBackColor = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+
+struct PDepartmentDetails: View {
+    @State var isDepartmentSelected : Bool = true
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading){
+                Text("Booking\nConsultation").font(.system(size: 35 , weight : .bold)).padding(.bottom,40)
+
+                Text("Choose Type of Doctor")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 20))
+                
+                    .padding(.bottom , 20)
+                HStack (spacing: 12){
+                    RoundedRectangle(cornerRadius: 60)
+                        
+                        .foregroundColor(isDepartmentSelected == false ? Color(doctorTypeBgColor) : Color(color))
+                        .frame(width: 150 , height: 50)
+                        
+                        .overlay( Text("General").font(.custom("Averta-Semibold", size: 14)).foregroundColor(isDepartmentSelected == false ? .white : .black))
+//                            .background {
+//                                RoundedRectangle(cornerRadius: 30)
+////                                        .stroke(Color.secondary, lineWidth: 4)
+//                            }
+                    RoundedRectangle(cornerRadius: 60)
+                        
+                        .foregroundColor(isDepartmentSelected == true ? Color(doctorTypeBgColor) : Color(color))
+                        .frame(width: 150 , height: 50)
+                        
+                        .overlay( Text("Specialist")
+                            .font(.custom("Averta-Semibold", size: 14))
+                            .foregroundColor(isDepartmentSelected == true ? .white : .black))
+//                            .background {
+//                                RoundedRectangle(cornerRadius: 30)
+//                                        .stroke(Color(doctorTypeBgColor), lineWidth: 4)
+//                            }
+
+                }.padding(.bottom , 60)
+                    
+                Text("About Department")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 20))
+                    .padding(.bottom,30)
+                
+                HStack {
+                    Circle().fill(Color(imgBackColor)).frame(width: 75 , height: 75)
+                    VStack(alignment : .leading , spacing: 8){
+                        Text("Oncology Department").fontWeight(.bold)
+                        Text("The oncology department in a hospital is dedicated to the diagnosis, treatment, and management of cancer patients.")
+                    }.padding(.init(top: 0, leading: 8, bottom: 0, trailing: 8))
+                }
+                .padding(.init(top: 24, leading: 16, bottom: 24, trailing: 16))
+                .background(RoundedRectangle(cornerRadius: 15).fill(Color(deptBackColor)))
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Continue" )
+                        .font(.custom("Averta-Semibold", size: 16))
+                }.padding()
+                    .padding(.horizontal,122)
+                    .background(Color(buttonColor)
+                        .cornerRadius(10))
+                    .foregroundColor(.white)
+
+                
+                    
+           
+            
+//            SpecialistModelView(isShowing: $showModel)
+            }.padding(24)
+            
+        }
+//                .navigationTitle(Text("Booking Consultation"))
+    }
+    }
+
+
+struct PDepartmentDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PDepartmentDetails()
+    }
+}
