482ebaa666c598df99a9f355c50ea999a6b9d8c6
Tanvi129 <tanvigupta129@gmail.com>
2023-04-24 11:53:22 +0530

    Basic Initialisation

---
+++ Zen-u/Resources/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Zen-u
+//
+//  Created by Tanvi Gupta on 21/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+var color = #colorLiteral(red: 0.9625412822, green: 0.9625412822, blue: 0.9625412822, alpha: 1)
+var hintTextColor = #colorLiteral(red: 0.7137254902, green: 0.7019607843, blue: 0.7019607843, alpha: 1)
+var doctorTypeBgColor = #colorLiteral(red: 0.09411764706, green: 0.4666666667, blue: 0.9490196078, alpha: 1)
+var buttonColor = #colorLiteral(red: 0.1882352941, green: 0.5333333333, blue: 1, alpha: 1)
--- Zen-u/Views/Doctor/Screens/DSchedule.swift
+++ Zen-u/Views/Doctor/Screens/DSchedule.swift
@@ -6,10 +6,49 @@
 //
 
 import SwiftUI
-
+let months = ["January" , "February", "March" , "April" , "May" , "June" , "July" , "August" , "September" ,"October" , "November" , "December"]
 struct DSchedule: View {
+    @State var selectedMonth: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+         
+        NavigationView(){
+            VStack{
+                ScrollView(.horizontal) {
+                            // 2
+                            LazyHStack {
+                                ForEach(0...11, id: \.self) {
+                                    index in
+                                    Button {
+                                        self.selectedMonth = index
+                                    } label: {
+                                        Text(months[index]).foregroundColor(self.selectedMonth == index ? Color(buttonColor): Color.gray)
+                                    }
+
+                                    
+                                        
+                                }
+                                    
+                            }
+                }.frame(height: 20)
+                VStack{
+                    HStack{
+                        ScrollView(.vertical) {
+                                    // 2
+                                    LazyVStack {
+                                        ForEach(1...31, id: \.self) { index in
+                                        
+                                                
+                                                }
+                                            
+                                    }
+                                }
+                    }
+                }
+                Spacer()
+            }.padding(24)
+            .navigationTitle("Schedule")
+        }
     }
 }
 
