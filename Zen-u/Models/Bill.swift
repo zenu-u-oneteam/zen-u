8788b32a93b3255b02b471cca00ed5359d2363fc
Inzamam <i.haqcs@gmail.com>
2023-04-24 16:44:44 +0530

    HMS-167 Bill Data Model Fix

--- Zen-u/Models/Bill.swift
+++ Zen-u/Models/Bill.swift
@@ -10,7 +10,6 @@ import Foundation
 struct Bill: Hashable, Codable, Identifiable {
     var id: Int
     var timestamp: DateComponents
-    var appointmentId: Int
     var amount: Float
     var status: Bool
 }
