00a6f83d4d1d831378e4dcba5d4da16864cf557a
Prakhar Singh <prakharsing7@gmail.com>
2023-04-26 10:18:10 +0530

    HMS-204 bug fixes

--- Zen-u/Views/Patient/Screens/PBookingAppointments.swift
+++ Zen-u/Views/Patient/Screens/PBookingAppointments.swift
@@ -6,10 +6,93 @@
 //
 
 import SwiftUI
+import UIKit
+
 
 struct PBookingAppointments: View {
+    @State var symtomText: String = ""
+    @State var showModel: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                VStack(alignment: .leading){
+                    
+                    Text("Choose Type of Doctor")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                        .padding(.bottom , 20)
+                    
+                    HStack (spacing: 16){
+                        RoundedRectangle(cornerRadius: 60)
+                        
+                            .foregroundColor(showModel == false ? Color("Accent") : Color("Secondary"))
+                            .frame(width: 150 , height: 50)
+                            .overlay(Text("General")
+                                .font(.callout)
+                                .foregroundColor(showModel == false ? .white : Color("Heading")))
+                        
+                        RoundedRectangle(cornerRadius: 60)
+                        
+                            .foregroundColor(showModel == true ? Color("Accent") : Color("Secondary"))
+                            .frame(width: 150 , height: 50)
+                            .overlay(Text("Specialist")
+                                .font(.callout)
+                                .foregroundColor(showModel == true ? .white : Color("Heading")))
+                        
+                    }
+                    .padding(.bottom , 60)
+                    .onTapGesture {
+                        showModel = true
+                        print("Tapped")
+                    }
+                    Text("Specify Reason (if Any)")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                        .padding(.bottom,30)
+                    
+                    ZStack (alignment: .leading) {
+                        
+                        TextEditor(text: $symtomText)
+                            .font(.body.weight(.light))
+                            .onTapGesture {
+                                if symtomText == "Mention any symptoms..." {
+                                    symtomText = ""
+                                }
+                            }
+                        
+                        if symtomText.isEmpty {
+                            Text("Mention any symptoms...")
+                                .foregroundColor(Color("Heading"))
+                                .font(.body.weight(.light))
+                                .padding(.horizontal, 4)
+                                .padding(.top, -76)
+                        }
+                    }
+                    .padding()
+                    .frame(height: 200.0)
+                    .colorMultiply(Color("Secondary"))
+                    .background(Color("Secondary"))
+                    .cornerRadius(26)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 26)
+                            .stroke(Color("Secondary"), lineWidth: 1)
+                    )
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        TabButton(text: "Continue")
+                    }
+                    Spacer()
+                }
+                .padding(24)
+                SpecialistModelView(isShowing: $showModel)
+            }
+            .navigationTitle(Text("Booking Consultation"))
+        }
+        
     }
 }
 
