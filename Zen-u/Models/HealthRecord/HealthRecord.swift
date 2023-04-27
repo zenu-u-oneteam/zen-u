00bd504f99a8d6fb3c184d9ceb805487bbd03bcd
Aindrila Ray <aindrilaray0803@gmail.com>
2023-04-27 12:33:43 +0530

    HMS-209 Initial Setup done, document picker yet to be implemented

--- Zen-u/Models/HealthRecord/HealthRecord.swift
+++ Zen-u/Models/HealthRecord/HealthRecord.swift
@@ -8,9 +8,16 @@
 import Foundation
 struct HealthRecord: Hashable, Codable, Identifiable {
     var id: Int
+    var name: String
     var appointmentTime: DateComponents
     var patient: Patient
     var doctor: Doctor
-    var type: HealthRecordType
+    var type: HealthCategory
     var document: String
+}
+enum HealthCategory: String, CaseIterable, Codable {
+    case prescription = "Prescription"
+    case labreports = "Lab Reports"
+    case vaccinationreports = "Vaccination Reports"
+    case hospitalization = "Hospitalization"
 }
