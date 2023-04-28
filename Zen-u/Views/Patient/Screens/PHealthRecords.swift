6c78aa183a4f434a7244e39089165df514f4b2f6
Aindrila Ray <aindrilaray0803@gmail.com>
2023-04-28 13:13:32 +0530

    HMS-214 Health Records and Health Records Category wise view page connected

--- Zen-u/Views/Patient/Screens/PHealthRecords.swift
+++ Zen-u/Views/Patient/Screens/PHealthRecords.swift
@@ -10,65 +10,74 @@ import SwiftUI
 struct PHealthRecords: View {
     var tableHeadings: [HealthRecordType]
     @State private var ButtonClicked = "Prescriptions"
+    @State private var isActive: Bool = false
     var body: some View {
-        VStack{
-            
-            //            HeaderView().padding(.all, 10)
-            //                .padding(.top, 30)
-            //                Spacer(minLength: 60)
-            VStack(alignment: .leading){
-                Text("All Health Documents").font(.system(size: 20)).bold()
-                    .hLeading()
-                    .padding(.all, 20)
-                    .padding(.top, 40)
-                    .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
-                ZStack{
-                    Rectangle()
-                        .frame(width: 346, height: 340)
-                        .foregroundColor(Color("Secondary"))
-                        .cornerRadius(15)
-                        .hCenter()
-                        .padding(.horizontal,20)
-                    
-                    VStack(alignment: .center, spacing: 16){
+        NavigationStack{
+            VStack{
+                VStack(alignment: .leading){
+                    Text("All Health Documents").font(.system(size: 20)).bold()
+                        .hLeading()
+                        .padding(.all, 20)
+                        .padding(.top, 40)
+                        .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 346, height: 340)
+                            .foregroundColor(Color("Secondary"))
+                            .cornerRadius(15)
+                            .hCenter()
+                            .padding(.horizontal,20)
                         
-                        ForEach(tableHeadings, id: \.self) { item in
-                            Button {
-                                
-                                ButtonClicked = item.title
-                                print(ButtonClicked)
-                                
-                            } label: {
-                                HStack(alignment: .center, spacing: 12){
+                        VStack(alignment: .center, spacing: 16){
+                            
+                            ForEach(tableHeadings, id: \.self) { item in
+                                Button {
                                     
-                                    Image(systemName: item.image).resizable()
-                                        .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
-                                        .frame(width: 40, height: 40)
+                                    ButtonClicked = item.title
+                                    print(ButtonClicked)
                                     
-                                    VStack(alignment: .leading){
-                                        Text(item.title).font(.system(size: 17)).bold()
-                                            .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
-                                            .padding(1)
-                                        Text(item.description).font(.system(size: 15)).foregroundColor(Color.gray)
+                                    
+                                } label: {
+                                    NavigationLink(destination: PHealthRecordsUploadView(healthrecord: [
+                                        HealthRecord(id: 1, name: "Lipid Profile", appointmentTime: DateComponents(year: 2023, month: 3, day: 12, hour: 09, minute: 34, second: 0) , patient: Patient(id: 1, age: 28 , gender: .female, bloodGroup: "AB+", height: 160.0, weight: 80), doctor: Doctor(id: 1, age: 65, gender: .male), type: .labreports, document: ""),
+                                        HealthRecord(id: 1, name: "Complete Blood Count", appointmentTime: DateComponents(year: 2022, month: 12, day: 4, hour: 16, minute: 34, second: 0) , patient: Patient(id: 1, age: 28 , gender: .female, bloodGroup: "AB+", height: 160.0, weight: 80), doctor: Doctor(id: 1, age: 65, gender: .male), type: .labreports, document: ""),
+                                        HealthRecord(id: 1, name: "Cholestrol-Serum", appointmentTime: DateComponents(year: 2022, month: 10, day: 8, hour: 13, minute: 34, second: 0) , patient: Patient(id: 1, age: 28 , gender: .female, bloodGroup: "AB+", height: 160.0, weight: 80), doctor: Doctor(id: 1, age: 65, gender: .male), type: .labreports, document: ""),
+                                        HealthRecord(id: 1, name: "H1PVC Test", appointmentTime: DateComponents(year: 2021, month: 12, day: 4, hour: 16, minute: 34, second: 0) , patient: Patient(id: 1, age: 28 , gender: .female, bloodGroup: "AB+", height: 160.0, weight: 80), doctor: Doctor(id: 1, age: 65, gender: .male), type: .labreports, document: ""),
+                                        HealthRecord(id: 1, name: "Gall Bladder Test", appointmentTime: DateComponents(year: 2021, month: 12, day: 4, hour: 16, minute: 34, second: 0) , patient: Patient(id: 1, age: 28 , gender: .female, bloodGroup: "AB+", height: 160.0, weight: 80), doctor: Doctor(id: 1, age: 65, gender: .male), type: .labreports, document: "")
+                                    ], heading: item.title) ) {
+                                        HStack(alignment: .center, spacing: 12){
+                                            
+                                            Image(systemName: item.image).resizable()
+                                                .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
+                                                .frame(width: 40, height: 40)
+                                            
+                                            VStack(alignment: .leading){
+                                                Text(item.title).font(.system(size: 17)).bold()
+                                                    .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
+                                                    .padding(1)
+                                                Text(item.description).font(.system(size: 15)).foregroundColor(Color.gray)
+                                                
+                                            }
+                                        }
+                                        Image(systemName: "chevron.right").foregroundColor(Color.gray).padding(.leading, 35)
                                         
-                                    }
-                                    Image(systemName: "chevron.right").foregroundColor(Color.gray).padding(.leading, 35)
+                                        //                                        .position(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+                                        //                                                }
+                                        
+                                    }.padding(.leading, 10)//tableviewcell
                                     
-                                    //                                        .position(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
-                                    //                                                }
-                                    
-                                }.padding(.leading, 10)//tableviewcell
-                                
-                            }   
-                            Divider().frame(width: 314)
-                                .padding(.leading, 30)
-                        }
-                    }//tableView
-                }          
-            }
-            Spacer(minLength: 200)
-            
-        }//main VStack
+                                }
+                                Divider().frame(width: 314)
+                                    .padding(.leading, 30)
+                            }
+                        }//tableView
+                    }
+                }
+                Spacer(minLength: 200)
+                
+            }//main VStack
+            .navigationTitle("Health Records")
+        }
     }
     func HeaderView() -> some View {
         HStack{
