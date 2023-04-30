811b1e18423383ae87d85cc707824e3f2db526ed
Tanvi129 <tanvigupta129@gmail.com>
2023-05-01 03:09:07 +0530

    HMS-205 screens connected

--- Zen-u/Views/Patient/Screens/PBooking/PBookingAppointments.swift
+++ Zen-u/Views/Patient/Screens/PBooking/PBookingAppointments.swift
@@ -10,89 +10,142 @@ import SwiftUI
 struct PBookingAppointments: View {
     @State var symtomText: String = ""
     @State var showModel: Bool = false
+    @State var selectedConsltType : consltType = consltType.general
+    @State var isDeptSelected : Bool = false
+    @State var selectedDeptText : String = "none"
+    
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading){
-                
-                Text("Choose Type of Doctor")
-                    .fontWeight(.semibold)
-                    .font(.system(size: 20))
-                    .padding(.bottom , 20)
-                
-                HStack (spacing: 16){
-                    RoundedRectangle(cornerRadius: 60)
+        NavigationView() {
+            ZStack {
+                VStack(alignment: .leading){
                     
-                        .foregroundColor(showModel == false ? Color("Accent") : Color("Secondary"))
-                        .frame(width: 150 , height: 50)
-                        .overlay(Text("General")
-                            .font(.callout)
-                            .foregroundColor(showModel == false ? .white : Color("Heading")))
+                    Text("Choose Type of Doctor")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                        .padding(.bottom , 20)
                     
-                    RoundedRectangle(cornerRadius: 60)
+                    HStack (spacing: 16){
+                        RoundedRectangle(cornerRadius: 60)
+                        
+                            .foregroundColor(selectedConsltType == consltType.general ? Color("Accent") : Color("Secondary"))
+                            .frame(width: 150 , height: 50)
+                            .overlay(Text("General")
+                                .font(.callout)
+                                .foregroundColor(selectedConsltType == consltType.general ? .white : Color("Heading"))).onTapGesture {
+                                    selectedConsltType = consltType.general
+                                    isDeptSelected = false
+                                    selectedDeptText = "none"
+                                }
+                        RoundedRectangle(cornerRadius: 60)
+                        
+                            .foregroundColor(selectedConsltType == consltType.specailist ? Color("Accent") : Color("Secondary"))
+                            .frame(width: 150 , height: 50)
+                            .overlay(Text("Specialist")
+                                .font(.callout)
+                                .foregroundColor(selectedConsltType == consltType.specailist ? .white : Color("Heading"))).onTapGesture {
+                                    self.selectedConsltType = consltType.specailist
+                                    self.showModel = true
+                                }
+                        
+                    }
+                    .padding(.bottom , 60)
+                    .onTapGesture {
+                        showModel = true
+                        print("Tapped")
+                    }
+                    if isDeptSelected {
+                        DeptSummary(heading: $selectedDeptText , description: "The oncology department in a hospital is dedicated to the diagnosis, treatment, and management of cancer patients.")
+                        
+
+                    }else{
+                        GeneralDetails(symtomText: $symtomText)
+                    }
                     
-                        .foregroundColor(showModel == true ? Color("Accent") : Color("Secondary"))
-                        .frame(width: 150 , height: 50)
-                        .overlay(Text("Specialist")
-                            .font(.callout)
-                            .foregroundColor(showModel == true ? .white : Color("Heading")))
                     
-                }
-                .padding(.bottom , 60)
-                .onTapGesture {
-                    showModel = true
-                    print("Tapped")
-                }
-                Text("Specify Reason (if Any)")
-                    .fontWeight(.semibold)
-                    .font(.system(size: 20))
-                    .padding(.bottom,30)
-                
-                ZStack (alignment: .leading) {
+                    Spacer()
                     
-                    TextEditor(text: $symtomText)
-                        .font(.body.weight(.light))
-                        .onTapGesture {
-                            if symtomText == "Mention any symptoms..." {
-                                symtomText = ""
-                            }
+                    Button {
+                        
+                    } label: {
+                        NavigationLink(destination: PScheduleSettings()) {
+                            TabButton(text: "Continue")
                         }
-                    
-                    if symtomText.isEmpty {
-                        Text("Mention any symptoms...")
-                            .foregroundColor(Color("Heading"))
-                            .font(.body.weight(.light))
-                            .padding(.horizontal, 4)
-                            .padding(.top, -76)
                     }
                 }
-                .padding()
-                .frame(height: 200.0)
-                .colorMultiply(Color("Secondary"))
-                .background(Color("Secondary"))
-                .cornerRadius(26)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 26)
-                        .stroke(Color("Secondary"), lineWidth: 1)
-                )
-                
-                Spacer()
-                
-                Button {
-                    
-                } label: {
-                    NavigationLink(destination: PScheduleSettings()) {
-                        TabButton(text: "Continue")
-                    }
-                }
-                Spacer()
+                .padding(24)
+                SpecialistModelView(isShowing: $showModel , isDeptSelected: $isDeptSelected, selectedConsltType : $selectedConsltType , selectedDeptText : $selectedDeptText)
             }
-            .padding(24)
-            SpecialistModelView(isShowing: $showModel)
+            .navigationTitle("Booking Consultation")
         }
-        .navigationTitle("Booking Consultation")
+    }
+}
+enum consltType{
+    case general
+    case specailist
+}
+
+struct DeptSummary: View {
+    @Binding var heading : String
+    @State var description : String
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("About Department")
+                                .fontWeight(.semibold)
+                                .font(.system(size: 20))
+                                .padding(.bottom,30)
+                            
+                            HStack {
+                                Circle().fill(Color("Subheadings")).frame(width: 75 , height: 75)
+                                VStack(alignment : .leading , spacing: 8){
+                                    Text("\(heading) Department").fontWeight(.bold)
+                                    Text(description)
+                                }.padding(.init(top: 0, leading: 8, bottom: 0, trailing: 8))
+                            }
+                            .padding(.init(top: 24, leading: 16, bottom: 24, trailing: 16))
+                            .background(RoundedRectangle(cornerRadius: 15).fill(Color("Secondary")))
+        }
     }
 }
 
+struct GeneralDetails: View {
+    @Binding var symtomText: String
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("Specify Reason (if Any)")
+                .fontWeight(.semibold)
+                .font(.system(size: 20))
+                .padding(.bottom,30)
+            
+            ZStack (alignment: .leading) {
+                
+                TextEditor(text: $symtomText)
+                    .font(.body.weight(.light))
+                    .onTapGesture {
+                        if symtomText == "Mention any symptoms..." {
+                            symtomText = ""
+                        }
+                    }
+                
+                if symtomText.isEmpty {
+                    Text("Mention any symptoms...")
+                        .foregroundColor(Color("Heading"))
+                        .font(.body.weight(.light))
+                        .padding(.horizontal, 4)
+                        .padding(.top, -76)
+                }
+            }
+            .padding()
+            .frame(height: 200.0)
+            .colorMultiply(Color("Secondary"))
+            .background(Color("Secondary"))
+            .cornerRadius(26)
+            .overlay(
+                RoundedRectangle(cornerRadius: 26)
+                    .stroke(Color("Secondary"), lineWidth: 1)
+            )
+        }
+    }
+}
 struct PBookingAppointments_Previews: PreviewProvider {
     static var previews: some View {
         PBookingAppointments()
--- Zen-u/Views/Patient/UIComponents/SpecialistModelView.swift
+++ Zen-u/Views/Patient/UIComponents/SpecialistModelView.swift
@@ -7,30 +7,46 @@
 
 import SwiftUI
 
-let departments = ["Skin Specialist" , "Neurology" , "Oncology", "Allergist and Clinical Immunologist" , "Psychiatry" ,"Sexual Health" ,"Gyanecology", "ENT" , "Cardiology" , "Diabetology" , "Othopaedics" , "Urology" , "Pediatrics" ]
+
 
 struct SpecialistModelView: View {
-    @Binding var isShowing: Bool
+    @Binding var isShowing : Bool
+    @Binding var isDeptSelected : Bool
+    @Binding var selectedConsltType : consltType
+    @Binding var selectedDeptText : String
     @State private var offset: CGFloat = 200.0
+    
+   
+    
     var body: some View {
         ZStack(alignment: .bottom){
             if isShowing {
                 Color.black.opacity(0.3).ignoresSafeArea()
                     .onTapGesture {
+                        
+                        
+                        selectedConsltType = consltType.general
+                        isDeptSelected = false
                         isShowing = false
+                        selectedDeptText = "none"
                     }
-                SpecalistContent(isShowing: $isShowing)
+                SpecalistContent(isShowing: $isShowing , isDeptSelected: $isDeptSelected  ,selectedDeptText : $selectedDeptText )
             }
             
         }
         .frame(maxWidth: .infinity ,maxHeight: .infinity ,alignment: .bottom)
-            .ignoresSafeArea()
-            .animation(Animation.easeInOut, value: offset)
+        .ignoresSafeArea()
+        .animation(Animation.easeInOut, value: offset)
     }
 }
 
 struct SpecalistContent: View {
     @Binding var isShowing : Bool
+    @Binding var isDeptSelected : Bool
+    @Binding var selectedDeptText : String
+    
+    let departments = ["Skin Specialist" , "Neurology" , "Oncology", "Allergist and Clinical Immunologist" , "Psychiatry" ,"Sexual Health" ,"Gyanecology", "ENT" , "Cardiology" , "Diabetology" , "Othopaedics" , "Urology" , "Pediatrics" ]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             
@@ -42,71 +58,106 @@ struct SpecalistContent: View {
                         .frame(width: 40, height: 40)
                     
                 ).onTapGesture {
+                    isDeptSelected = false
                     isShowing = false
+                    selectedDeptText = "none"
                 }
                 
                 
             }.padding(24)
             HStack(spacing: 8){
-                DepartmentLabel(labelText: departments[0])
-                DepartmentLabel(labelText: departments[1])
-                DepartmentLabel(labelText: departments[2])
+                DepartmentLabel(labelText: departments[0], isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText )
+                DepartmentLabel(labelText: departments[1] , isDeptSelected: $isDeptSelected , selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[2] , isDeptSelected: $isDeptSelected , selectedDeptText: $selectedDeptText)
+                
+                
             }
             HStack {
-                DepartmentLabel(labelText: departments[3])
-                DepartmentLabel(labelText: departments[4])
+                DepartmentLabel(labelText: departments[3],  isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[4], isDeptSelected: $isDeptSelected , selectedDeptText: $selectedDeptText)
             }
             HStack(spacing: 8){
-                DepartmentLabel(labelText: departments[5])
-                DepartmentLabel(labelText: departments[6])
-                DepartmentLabel(labelText: departments[7])
+                DepartmentLabel(labelText: departments[5],  isDeptSelected: $isDeptSelected , selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[6], isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[7], isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText)
             }
             HStack(spacing: 8){
-                DepartmentLabel(labelText: departments[8])
-                DepartmentLabel(labelText: departments[9])
-                DepartmentLabel(labelText: departments[10])
+                DepartmentLabel(labelText: departments[8], isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[9], isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[10], isDeptSelected: $isDeptSelected, selectedDeptText: $selectedDeptText)
             }
             HStack() {
-                DepartmentLabel(labelText: departments[11])
-                DepartmentLabel(labelText: departments[12])
+                DepartmentLabel(labelText: departments[11], isDeptSelected: $isShowing, selectedDeptText: $selectedDeptText)
+                DepartmentLabel(labelText: departments[12], isDeptSelected: $isShowing, selectedDeptText: $selectedDeptText)
             }
-        
+            
             Spacer()
+            Button {
+                if isDeptSelected {
+                    isShowing = false
+                    
+                }
+               
+            }
+        label: {
+            Text("OK").foregroundColor(isDeptSelected ? .white : .black )
+        }
+        .disabled(!isDeptSelected)
+            .padding()
+            .padding(.horizontal,122)
+            .background(Color(isDeptSelected ? "Accent" : "Secondary" )
+                .cornerRadius(10))
+            .foregroundColor(.white)
+            .hCenter()
+            .padding(.bottom , 30)
+            
             
             
         }
         .padding(16)
-            .frame(height: 450)
-            .frame(maxWidth: .infinity)
-            .background(
-                
-                ZStack {
-                    
-                    RoundedRectangle(cornerRadius: 30)
-                    Rectangle().frame(height: 450/2)
-                }
-                    .foregroundColor(.white)
-            )
+        .frame(height: 510)
+        .frame(maxWidth: .infinity)
+        .background(
             
-            .transition(.move(edge: .bottom))
+            ZStack {
+                
+                RoundedRectangle(cornerRadius: 30)
+                Rectangle().frame(height: 450/2)
+            }
+                .foregroundColor(.white)
+        )
+        
+        .transition(.move(edge: .bottom))
     }
     
 }
-    
-    struct SpecialistModelView_Previews: PreviewProvider {
-        static var previews: some View {
-            //        SpecialistModelView(isShowing: .constant(true))
-            PBookingAppointments()
-        }
+
+struct SpecialistModelView_Previews: PreviewProvider {
+    static var previews: some View {
+        //        SpecialistModelView(isShowing: .constant(true))
+        PBookingAppointments()
     }
+}
 
 struct DepartmentLabel: View {
     @State var labelText : String
+    @Binding var isDeptSelected: Bool
+    @Binding var selectedDeptText : String
+    
     var body: some View {
-        Button{} label: { Text(labelText)
+        Button{
+            
+            if selectedDeptText == labelText {
+                selectedDeptText = "none"
+                isDeptSelected = false
+            }else{
+                selectedDeptText = labelText
+                isDeptSelected = true
+            }
+        } label: { Text(labelText)
                 .font(.system(size: 13 , weight: .semibold))
                 .padding(16)
-            .foregroundColor(.black).background(RoundedRectangle(cornerRadius: 50).fill(Color("Primary")).frame(height: 44)) }
+            .foregroundColor(.black).background(RoundedRectangle(cornerRadius: 50).fill(Color(selectedDeptText == labelText ? "Accent" : "Priamry" )).frame(height: 44)) }
         
     }
 }
