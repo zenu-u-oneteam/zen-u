cde2ce94bd21d5b35e1b5fa6759e3f5bdb7c510d
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-05-02 07:20:52 +0530

    HMS-126 create tabs

--- Zen-u/Views/Patient/Screens/PReminders.swift
+++ Zen-u/Views/Patient/Screens/PReminders.swift
@@ -18,43 +18,31 @@ struct PReminders: View {
     }()
     
     var body: some View {
-        VStack{
+        VStack(spacing: 20){
             Text("\(dateFormatter.string(from: Date()))").font(.largeTitle).fontWeight(.black)
-            Spacer()
-            ScrollView(.vertical,showsIndicators: false) {
-                LazyVStack(spacing: 30, pinnedViews: [.sectionHeaders]) {
-                    
-                    Section{
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            
-                            HStack(spacing: 12) {
-                                
-                                ForEach(appointmentViewModel.currentWeek, id:  \.self){day in
-                                   
-                                    VStack{
-                                    
+            LazyVStack(spacing: 30, pinnedViews: [.sectionHeaders]) {
+                Section{
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 12) {
+                            ForEach(appointmentViewModel.currentWeek, id:  \.self){day in
+                                VStack{
                                     VStack(spacing: 10) {
-                                        
                                         Text(appointmentViewModel.extractDate(date: day, format: "dd"))
                                             .font(.system(size: 20))
                                             .fontWeight(.heavy)
-                                        
                                     }
                                     .frame(width: 60, height: 60)
                                     .background(
                                         ZStack {
                                             if appointmentViewModel.isToday(date: day){
                                                 Circle()
-                                                    .fill(.blue)
+                                                    .fill(Color.accentColor)
                                                     .cornerRadius(20)
-                                                    .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
-                                                
+                                                //  .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
                                             } else {
                                                 Circle()
                                                     .fill((Color("Secondary")))
                                                     .cornerRadius(20)
-                                                
                                             }
                                         }
                                     )
@@ -62,24 +50,26 @@ struct PReminders: View {
                                     .onTapGesture {
                                         appointmentViewModel.currentDay = day
                                     }
-                                        VStack{
-                                            Text(appointmentViewModel.extractDate(date: day, format: "EEE"))
-                                                .font(.system(size: 14)).fontWeight(.bold)
-                                        }.foregroundStyle(appointmentViewModel.isToday(date: day) ? .primary : .secondary)
-                                            .foregroundColor(appointmentViewModel.isToday(date: day) ? .accentColor : .black)
+                                    VStack{
+                                        Text(appointmentViewModel.extractDate(date: day, format: "EEE"))
+                                            .font(.system(size: 14)).fontWeight(.bold)
                                     }
-                                    
+                                    .foregroundStyle(appointmentViewModel.isToday(date: day) ? .primary : .secondary)
+                                    .foregroundColor(appointmentViewModel.isToday(date: day) ? .accentColor : .black)
                                 }
                             }
                         }
-                        
                     }
+                    
                 }
+            }
+            Spacer()
+            //sort buttons
+            HStack(spacing: 20){
                 
             }
-            
-        }.ignoresSafeArea(.container, edges: .trailing)
-            .padding(20)
+        }
+        .padding(20)
         
     }
     
