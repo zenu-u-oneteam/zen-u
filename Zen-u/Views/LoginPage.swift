caf28edfccc0522c945daef975579581ae411ca8
Prakhar Singh <prakharsing7@gmail.com>
2023-05-02 17:46:30 +0530

    HMS-225 & HMS-221

--- Zen-u/Views/LoginPage.swift
+++ Zen-u/Views/LoginPage.swift
@@ -56,153 +56,163 @@ struct LoginPage: View {
     }
     
     var body: some View {
-        ZStack {
-            VStack {
-                VStack(alignment: .leading, spacing: 10) {
-                    HStack {
-                        Image(systemName: "envelope")
+        ScrollView {
+            ZStack {
+                VStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        HStack {
+                            Image(systemName: "envelope")
+                                .foregroundColor(.black)
+                            TextField("Enter your email", text: $email)
+                                .font(.system(size: 17, weight: .light))
+                                .textInputAutocapitalization(.never)
+                                .autocorrectionDisabled(true)
+                                .keyboardType(.emailAddress)
+                        }
+                        .padding()
+                        .background(Color(.systemGray5))
+                        .cornerRadius(12)
+                    }
+                    .padding(.horizontal, 30)
+                    .padding(.vertical)
+                    
+                    HStack(alignment: .center, spacing: 13) {
+                        Image(systemName: "lock")
                             .foregroundColor(.black)
-                        TextField("Enter your email", text: $email)
+                        SecureField("Enter your password", text: $password)
                             .font(.system(size: 17, weight: .light))
                             .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled(true)
                     }
                     .padding()
                     .background(Color(.systemGray5))
                     .cornerRadius(12)
-                }
-                .padding(.horizontal, 30)
-                .padding(.vertical)
-                
-                HStack(alignment: .center, spacing: 13) {
-                    Image(systemName: "lock")
-                        .foregroundColor(.black)
-                    SecureField("Enter your password", text: $password)
-                        .font(.system(size: 17, weight: .light))
-                        .textInputAutocapitalization(.never)
-                }
-                .padding()
-                .background(Color(.systemGray5))
-                .cornerRadius(12)
-                
-                .padding(.horizontal, 30)
-                
-                HStack {
-                    Spacer()
-                    Text("Forgot Password?")
-                        .font(.subheadline)
-                        .foregroundColor(.blue)
-                }
-                .padding(.trailing, 30)
-                
-                Button {
-                    passwordLogin()
-                } label: {
-                    ActionButton(text: "Log in")
-                }
-                .padding(.top, 20)
-                                
-                HStack {
-                    Text("Don't have an account?")
-                        .foregroundColor(.gray)
-                    NavigationLink(
-                        destination: SignupPage(),
-                        label: {
-                            Text("Sign Up.")
-                                .foregroundColor(.blue)
-                                .fontWeight(.semibold)
-                        })
-                }
-                .padding()
-                
-                HStack {
-                    Rectangle()
-                        .frame(height: 1)
-                        .foregroundColor(.gray)
-                        .padding(.leading, 20)
-                        .padding(.trailing, 15)
-                    Text("OR")
-                        .foregroundColor(.gray)
-                        .fontWeight(.regular)
-                        .font(.system(size: 20))
-                    Rectangle()
-                        .frame(height: 1)
-                        .foregroundColor(.gray)
-                        .padding(.trailing, 20)
-                        .padding(.leading, 15)
-                }
-                .padding(.vertical)
-                .padding(.bottom)
-                                
-                VStack(spacing: 20) {
-                    Button(action: {
-                        // Perform Google sign-in action here
-                        appState.rootViewId = UUID()
-                    }) {
-                        HStack {
-                            Image("google")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 25, height: 25)
-                            Text("Sign in with Google")
-                                .fontWeight(.medium)
-                                .foregroundColor(.black)
-                                .padding(.horizontal, 51)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                        .background(Color.white)
-                        .cornerRadius(25)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.gray, lineWidth: 0.5)
-                        )
+                    
+                    .padding(.horizontal, 30)
+                    
+                    HStack {
+                        Spacer()
+                        Text("Forgot Password?")
+                            .font(.subheadline)
+                            .foregroundColor(.blue)
                     }
-                    Button(action: {
-                        // Perform Apple sign-in action here
-                    }) {
-                        HStack {
-                            Image("apple")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 25, height: 25)
-                            Text("Sign in with Apple")
-                                .fontWeight(.medium)
-                                .foregroundColor(.black)
-                                .padding(.horizontal, 56)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                        .background(Color.white)
-                        .cornerRadius(25)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.gray, lineWidth: 0.5)
-                        )
+                    .padding(.trailing, 30)
+                    
+                    Button {
+                        passwordLogin()
+                    } label: {
+                        ActionButton(text: "Log in")
                     }
-                    Button(action: {
-                        // Perform Facebook sign-in action here
-//                        popToRootViewController(animated: false)
-                    }) {
-                        HStack {
-                            Image("facebook")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 25, height: 25)
-                            Text("Sign in with Facebook")
-                                .fontWeight(.medium)
-                                .foregroundColor(.black)
-                                .padding(.horizontal, 40)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                        .background(Color.white)
-                        .cornerRadius(25)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.gray, lineWidth: 0.5)
-                        )
+                    .padding(.top, 20)
+                    
+                    HStack {
+                        Text("Don't have an account?")
+                            .foregroundColor(.gray)
+                        NavigationLink(
+                            destination: SignupPage(),
+                            label: {
+                                Text("Sign Up.")
+                                    .foregroundColor(.blue)
+                                    .fontWeight(.semibold)
+                            })
                     }
+                    .padding()
+                    
+                    HStack {
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundColor(.gray)
+                            .padding(.leading, 20)
+                            .padding(.trailing, 15)
+                        Text("OR")
+                            .foregroundColor(.gray)
+                            .fontWeight(.regular)
+                            .font(.system(size: 20))
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundColor(.gray)
+                            .padding(.trailing, 20)
+                            .padding(.leading, 15)
+                    }
+                    .padding(.vertical)
+                    .padding(.bottom)
+                    
+                    VStack(spacing: 20) {
+                        Button(action: {
+                            // Perform Google sign-in action here
+                            appState.rootViewId = UUID()
+                        }) {
+                            HStack {
+                                Image("google")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 25, height: 25)
+                                Text("Sign in with Google")
+                                    .fontWeight(.medium)
+                                    .foregroundColor(.black)
+                                    .padding(.horizontal, 51)
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 50)
+                            .background(Color.white)
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.gray, lineWidth: 0.5)
+                            )
+                        }
+                        Button(action: {
+                            // Perform Apple sign-in action here
+                        }) {
+                            HStack {
+                                Image("apple")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 25, height: 25)
+                                Text("Sign in with Apple")
+                                    .fontWeight(.medium)
+                                    .foregroundColor(.black)
+                                    .padding(.horizontal, 56)
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 50)
+                            .background(Color.white)
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.gray, lineWidth: 0.5)
+                            )
+                        }
+                        Button(action: {
+                            // Perform Facebook sign-in action here
+                            //                        popToRootViewController(animated: false)
+                        }) {
+                            HStack {
+                                Image("facebook")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 25, height: 25)
+                                Text("Sign in with Facebook")
+                                    .fontWeight(.medium)
+                                    .foregroundColor(.black)
+                                    .padding(.horizontal, 40)
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 50)
+                            .background(Color.white)
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.gray, lineWidth: 0.5)
+                            )
+                        }
+                    }
+                    .padding(.horizontal, 30)
                 }
-                .padding(.horizontal, 30)
+                
             }
-            .navigationBarTitle("zen-u", displayMode: .inline)
+        }
+        .ignoresSafeArea(.keyboard)
+        .navigationBarTitle("zen-u", displayMode: .inline)
+        .onTapGesture {
+            self.hideKeyboard()
         }
     }
 }
